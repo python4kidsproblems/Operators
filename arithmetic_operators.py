1e5484d235f12f274b37a41fd55afc652fb87047
python4kidsproblems <54461624+python4kidsproblems@users.noreply.github.com>
2019-08-24 09:16:35 +0500

    Add files via upload

---
+++ arithmetic_operators.py
@@ -0,0 +1,33 @@
+# Arithmetic Operators Practice Problems
+
+# How to use this file:
+# Copy this to your python (.py) file, and write your code in the given space
+
+################################### Q1 ###################################
+# Write a program that that asks the user to enter the ages of their     #
+# mother, father, sister, and brother.                                   #
+# Using Arithmetic operators, find the sum and mean of the 4 ages.       #
+# Now, find the difference in the ages of the father and mother          #
+##########################################################################
+
+# your code goes here
+
+################################### Q2 ###################################
+# Write a pragram that asks the user to enter any two numbers and prints #
+# a. The sum of the two numbers.                                         #
+# b. The difference between the two numbers.                             #
+# c. The multiplication between the two numbers.                         #
+# d. The division between the two numbers.                               #
+# Your code should work for both decimal numbers and integes.            #
+##########################################################################
+
+# your code goes here
+
+################################### Q2 ###################################
+# Make a variable and store any 3 digit number in it.                    #
+# Your task is to reverse the 3 digits,                                  #
+# e.d 672 -> 276                                                         #
+# Hint, try converting to another data type.                             #
+##########################################################################
+
+# your code goes here
