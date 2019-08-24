d6e813e7369922bf447bf2fd88c892d56ff0739b
python4kidsproblems <54461624+python4kidsproblems@users.noreply.github.com>
2019-08-24 09:30:56 +0500

    Add files via upload

---
+++ comparison_operators.py
@@ -0,0 +1,24 @@
+# Comarison Operators Practice Problems
+
+# How to use this file:
+# Copy this to your python (.py) file, and write your code in the given space
+
+################################### Q1 ###################################
+# Write a program that asks the user,                                    #
+# "Are you older than 15? Answer True or Flase."                         #
+# Convert the user response to Bool and then check its type using the    #
+# type function.                                                         #
+# See what happpens if the user does not repond with True or False,     #
+# and replies with Yes or No instead. Will this program still work?      #
+##########################################################################
+
+# your code goes here
+
+################################### Q2 ###################################
+# Write a Truth Detector program that gives the User two numbers to      #
+# compare and tell which one is bigger. If the User answers correctly    #
+# print True and if not print False.                                     #
+# Hint: Notice that print(1==1) prints True and print(4<3) prints False. # 
+##########################################################################
+
+# your code goes here
